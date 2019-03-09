e917f81333b7b4c23ba8e4a8d882e6ed047979f0
Your Name <l>
2019-03-10 01:53:59 +0800

    sss

---
+++ byfmall-manager-web/src/main/java/com/byf/App.kt
@@ -0,0 +1,12 @@
+package com.byf
+
+/**
+ * Hello world!
+ *
+ */
+object App {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Hello World!")
+    }
+}
---
+++ byfmall-shop-web/src/main/java/com/byf/App.java
@@ -0,0 +1,13 @@
+package com.byf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
